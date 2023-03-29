113874be0dff66838caacbd62456e2d6f2f115f7
conanak99 <huyhoang8a5@gmail.com>
2023-03-29 16:21:05 +0800

    fix character form

--- src/pages/Character/CharacterForm.tsx
+++ src/pages/Character/CharacterForm.tsx
@@ -1,18 +1,38 @@
+import axios from "axios";
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
+import { axiosInstance, supabase } from "../../config";
 import { parseCharacter } from "../../services/character_parse";
 
 interface FormValues {
   import?: FileList;
+
+  avatar: string;
+
+  name: string;
+  description: string;
+  personality: string;
+  scenario: string;
+  example_dialogs: string;
+  first_message: string;
+
+  is_nsfw: boolean;
+  is_public: boolean;
 }
 
 export const CharacterForm = () => {
   const {
     register,
-    // handleSubmit,
     watch,
+    handleSubmit,
+    setValue,
     formState: { errors },
-  } = useForm<FormValues>({ defaultValues: {} });
+  } = useForm<FormValues>({
+    defaultValues: {
+      is_nsfw: false,
+      is_public: true,
+    },
+  });
 
   const importFile = watch("import");
 
@@ -23,8 +43,17 @@ export const CharacterForm = () => {
         console.log({ file });
 
         try {
-          const source = await parseCharacter(file);
-          console.log(source, source.character, source.image, source.json, source.character.tokens);
+          const { character, json, image } = await parseCharacter(file);
+          console.log(character, image, json);
+
+          if (character) {
+            setValue("name", character.name);
+            setValue("description", character.description);
+            setValue("personality", character.personality);
+            setValue("first_message", character.first_message);
+            setValue("example_dialogs", character.example_dialogs);
+            setValue("scenario", character.scenario);
+          }
         } catch (ex) {
           console.error(ex);
         }
@@ -34,9 +63,28 @@ export const CharacterForm = () => {
     run();
   }, [importFile]);
 
-  const onSubmit = () => {
-    console.log("hello");
-  };
+  const onSubmit = handleSubmit(async (values: FormValues) => {
+    const avatarImg = importFile?.[0];
+    let botAvatar = null;
+    if (avatarImg) {
+      const extension = avatarImg.name.substring(avatarImg.name.lastIndexOf(".") + 1);
+
+      const result = await supabase.storage
+        .from("bot-avatars")
+        .upload(`${crypto.randomUUID()}.${extension}`, avatarImg, {
+          cacheControl: "3600",
+          upsert: true,
+        });
+      botAvatar = result.data && result.data.path;
+    }
+
+    const result = await axiosInstance.post("/character", {
+      ...values,
+      avatar: botAvatar,
+    });
+
+    console.log(result.data);
+  });
 
   return (
     <div>
@@ -44,6 +92,14 @@ export const CharacterForm = () => {
       <form onSubmit={onSubmit}>
         <h3>Import TavernAI bot</h3>
         <input {...register("import")} type="file" />
+        <input {...register("name")} placeholder="Name" />
+        <input {...register("description")} placeholder="description" />
+        <input {...register("personality")} placeholder="personality" />
+        <input {...register("scenario")} placeholder="scenario" />
+        <textarea {...register("first_message")} placeholder="first_message" />
+        <textarea {...register("example_dialogs")} placeholder="example_dialogs" />
+        <input {...register("is_nsfw")} type="checkbox" /> Is NSFW Bot
+        <input {...register("is_public")} type="checkbox" /> Is Public Bot
         <input type="submit" value="Import" />
       </form>
     </div>
--- src/services/character-parse/character.ts
+++ src/services/character-parse/character.ts
@@ -34,73 +34,68 @@ type CharacterJsonObjKey = keyof CharacterJsonObject | keyof CaiCharacterJson;
 
 export class Character {
   name: string;
-  summary: string;
+  description: string;
   personality: string;
   scenario: string;
-  greeting: string;
-  examples: string;
+  first_message: string;
+  example_dialogs: string;
 
   get tokens() {
     return Tokenizer.count(
       this.name +
-        this.summary +
+        this.description +
         this.personality +
         this.scenario +
-        this.greeting +
-        this.examples
+        this.first_message +
+        this.example_dialogs
     );
   }
 
   #valid(object: CharacterJsonObject, key: CharacterJsonObjKey) {
-    if (!object || !object.hasOwnProperty(key) || !object[key]) return false;
+    if (!object || !object.hasOwnProperty(key)) return false;
 
     return true;
   }
 
   #sanitize(object: CharacterJsonObject, key: CharacterJsonObjKey) {
-    return this.#valid(object, key) ? (object[key] as string).trim() : "";
+    return (object[key] as string).trim() || "";
   }
 
   constructor(json: CharacterJsonObject | null) {
     if (!json) return;
 
     // Text Generation
-    if (this.#valid(json, "char_name"))
-      this.name = this.#sanitize(json, "char_name");
-    if (this.#valid(json, "char_persona"))
-      this.personality = this.#sanitize(json, "char_persona");
-    if (this.#valid(json, "world_scenario"))
-      this.scenario = this.#sanitize(json, "world_scenario");
+    if (this.#valid(json, "char_name")) this.name = this.#sanitize(json, "char_name");
+    if (this.#valid(json, "char_persona")) this.personality = this.#sanitize(json, "char_persona");
+    if (this.#valid(json, "world_scenario")) this.scenario = this.#sanitize(json, "world_scenario");
     if (this.#valid(json, "char_greeting"))
-      this.greeting = this.#sanitize(json, "char_greeting");
+      this.first_message = this.#sanitize(json, "char_greeting");
     if (this.#valid(json, "example_dialogue"))
-      this.examples = this.#sanitize(json, "example_dialogue");
+      this.example_dialogs = this.#sanitize(json, "example_dialogue");
 
     // TavernAI
     if (this.#valid(json, "name")) this.name = this.#sanitize(json, "name");
-    if (this.#valid(json, "personality"))
-      this.summary = this.#sanitize(json, "personality");
-    if (this.#valid(json, "description"))
-      this.personality = this.#sanitize(json, "description");
-    if (this.#valid(json, "scenario"))
-      this.scenario = this.#sanitize(json, "scenario");
-    if (this.#valid(json, "first_mes"))
-      this.greeting = this.#sanitize(json, "first_mes");
+    if (this.#valid(json, "personality")) this.description = this.#sanitize(json, "personality");
+    if (this.#valid(json, "description")) this.personality = this.#sanitize(json, "description");
+    console.log({ ah: this });
+
+    if (this.#valid(json, "scenario")) this.scenario = this.#sanitize(json, "scenario");
+
+    if (this.#valid(json, "first_mes")) this.first_message = this.#sanitize(json, "first_mes");
     if (this.#valid(json, "mes_example"))
-      this.examples = this.#sanitize(json, "mes_example");
+      this.example_dialogs = this.#sanitize(json, "mes_example");
 
     // CIA Character
     if (json.character) {
-      if (this.#valid(json.character, "name"))
-        this.name = this.#sanitize(json.character, "name");
+      if (this.#valid(json.character, "name")) this.name = this.#sanitize(json.character, "name");
       if (this.#valid(json.character, "title"))
-        this.summary = this.#sanitize(json.character, "title");
+        this.description = this.#sanitize(json.character, "title");
       if (this.#valid(json.character, "description"))
         this.personality = this.#sanitize(json.character, "description");
       if (this.#valid(json.character, "greeting"))
-        this.greeting = this.#sanitize(json.character, "greeting");
+        this.first_message = this.#sanitize(json.character, "greeting");
       if (this.#valid(json.character, "definition"))
-        this.examples = this.#sanitize(json.character, "definition");
+        this.example_dialogs = this.#sanitize(json.character, "definition");
     }
 
     // CIA History
@@ -108,13 +103,13 @@ export class Character {
       if (this.#valid(json.info.character, "name"))
         this.name = this.#sanitize(json.info.character, "name");
       if (this.#valid(json.info.character, "title"))
-        this.summary = this.#sanitize(json.info.character, "title");
+        this.description = this.#sanitize(json.info.character, "title");
       if (this.#valid(json.info.character, "description"))
         this.personality = this.#sanitize(json.info.character, "description");
       if (this.#valid(json.info.character, "greeting"))
-        this.greeting = this.#sanitize(json.info.character, "greeting");
+        this.first_message = this.#sanitize(json.info.character, "greeting");
       if (this.#valid(json.info.character, "definition"))
-        this.examples = this.#sanitize(json.info.character, "definition");
+        this.example_dialogs = this.#sanitize(json.info.character, "definition");
     }
   }
 }
