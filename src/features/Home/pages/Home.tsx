a0b667abe8f0cdfa7cd264535c0f33b0973fd614
conanak99 <huyhoang8a5@gmail.com>
2023-05-07 11:22:17 +0800

    switch to popular for now

--- src/features/Home/pages/Home.tsx
+++ src/features/Home/pages/Home.tsx
@@ -48,7 +48,7 @@ const TagContainer = styled.div`
 
 export const Home: React.FC = () => {
   const { profile, localData, updateLocalData } = useContext(AppContext);
-  const [segment, setSegment] = useState<Segment>("latest");
+  const [segment, setSegment] = useState<Segment>("popular");
   const tags = useTags();
 
   const params: SearchParams | undefined = useMemo(() => {
